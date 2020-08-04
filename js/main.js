697ab90ff09fd84eeab230620e91ac243a804efa
Ortiz David <49447702+soporte00@users.noreply.github.com>
2020-08-04 00:47:25 -0300

    se reparo el retardo al inicio de la aplicacion

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const clock = document.getElementById('clock')
 
-setInterval(()=>{ 
-
+const actualizeClock = ()=>{
 	let time = new Date()
-
 	clock.children.second.style.transform=`rotate(${ ( time.getSeconds() * 6 ) }deg)`;
 	clock.children.minute.style.transform=`rotate(${ ( time.getMinutes() * 6 ) }deg)`;
 	clock.children.hour.style.transform=`rotate(${ (time.getHours() * 30) + (time.getMinutes() * 0.5)   }deg)`;
+}
 
-}, 1000);
+actualizeClock();
+setInterval( actualizeClock, 1000);
